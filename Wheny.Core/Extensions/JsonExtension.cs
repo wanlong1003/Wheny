a1acb9b0690f133b675db90e0f1d5d7321d2399c
Wanlong Liu <wanlong1003@163.com>
2017-11-06 23:05:17 +0800

    添加json字符串的扩展

---
+++ Wheny.Core/Extensions/JsonExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace Wheny.Core
+{
+    public static class JsonExtension
+    {
+        /// <summary>
+        /// 对象转换为Json字符串
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string JsonSerialize(this object sender)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(sender);
+        }
+
+        /// <summary>
+        /// Json字符串反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sender"></param>
+        /// <returns></returns>
+        public static T JsonDeserialize<T>(this string sender)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(sender);
+        }
+
+
+    }
+}
