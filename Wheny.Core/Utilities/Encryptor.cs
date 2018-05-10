64a52474b56151634853559036291762ac90bc6b
Wanlong Liu <wanlong1003@163.com>
2018-05-10 18:15:26 +0800

    修改

--- Wheny.Core/Utilities/Encryptor.cs
+++ Wheny.Core/Utilities/Encryptor.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Wheny.Core.Utilities
+namespace Wheny.Core
 {
     public static class Encryptor
     {
